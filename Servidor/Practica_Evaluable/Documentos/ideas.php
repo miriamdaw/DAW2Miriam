6986c4590912191d9c60314054c114071ecc39cd
Miriam <miriamramosgonzalezdaw@gmail.com>
2023-11-20 17:19:06 +0100

    a

--- Servidor/Practica_Evaluable/Documentos/ideas.php
+++ Servidor/Practica_Evaluable/Documentos/ideas.php
@@ -1,27 +1,9 @@
 <?php
 /*
-Pagina web de clase de DAW para compartir
-fotos de nuestras mascotas 
-
-> Nombre de mascota
-> Pequeña descripcion
-> Tipo de mascota (gato, perro...)
-> Imagen de mascota
-
-
--- Reglas de negocio
-
-!> Nombre del dueño debe ser una cadena de
-   caracteres de hasta 20 caracteres, empezar en mayusculas
-   -> espacios, guiones, exclamaciones
-
-!> Descripcion debe contener al menos 50 caracteres
-
-!> Categorias son: perros, gatos,
-   tortugas, conejos, pajaros,
-   peces
-
-
- 
+email
+slider control
+date and time
+color picker
+radio
 */
 ?>
