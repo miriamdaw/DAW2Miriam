8b31113c2c4af84fa71431d3940c290576574ec6
Miriam Ramos <miriamramosgonzalezdaw@gmail.com>
2023-11-30 12:53:45 +0100

    servidor

--- Servidor/Ejercicios/Ejercicios_Clase/pedidos_restaurante/bd_tienda.php
+++ Servidor/Ejercicios/Ejercicios_Clase/pedidos_restaurante/bd_tienda.php
@@ -1,37 +1,89 @@
 <?php
-	function leer_config($nombre, $esquema)
-	{
-		$config = new DOMDocument();
-		$config->load($nombre);
-		$res = $config->schemaValidate($esquema);
-		if ($res === FALSE) {
-			throw new InvalidArgumentException("Revise fichero de configuración");
-		}
-		$datos = simplexml_load_file($nombre);
-		$ip = $datos->xpath("//ip");
-		$nombre = $datos->xpath("//nombre");
-		$usu = $datos->xpath("//usuario");
-		$clave = $datos->xpath("//clave");
-		$cad = sprintf("mysql:dbname=%s;host=%s", $nombre[0], $ip[0]);
-		$resul = [];
-		$resul[] = $cad;
-		$resul[] = $usu[0];
-		$resul[] = $clave[0];
-		return $resul;
+function leer_config($nombre, $esquema)
+{
+	$config = new DOMDocument();
+	$config->load($nombre);
+	$res = $config->schemaValidate($esquema);
+	if ($res === FALSE) {
+		throw new InvalidArgumentException("Revise fichero de configuración");
 	}
-	function comprobar_usuario($nombre, $clave)
-	{
-		//clave encriptada, hash y validate
-		$res = leer_config(dirname(__FILE__) . "/configuracion.xml", dirname(__FILE__) . "/configuracion.xsd");
-		$bd = new PDO($res[0], $res[1], $res[2]);
-		$ins = "select codigoRestaurante, correo from restaurantes where correo = '$nombre' 
+	$datos = simplexml_load_file($nombre);
+	$ip = $datos->xpath("//ip");
+	$nombre = $datos->xpath("//nombre");
+	$usu = $datos->xpath("//usuario");
+	$clave = $datos->xpath("//clave");
+	$cad = sprintf("mysql:dbname=%s;host=%s", $nombre[0], $ip[0]);
+	$resul = [];
+	$resul[] = $cad;
+	$resul[] = $usu[0];
+	$resul[] = $clave[0];
+	return $resul;
+}
+function comprobar_usuario($nombre, $clave)
+{
+	//clave encriptada, hash y validate
+	$res = leer_config(dirname(__FILE__) . "/configuracion.xml", dirname(__FILE__) . "/configuracion.xsd");
+	$bd = new PDO($res[0], $res[1], $res[2]);
+	$ins = "select codRes, correo from restaurantes where correo = '$nombre' 
 			and clave = '$clave'";
-		$resul = $bd->query($ins);
-		if ($resul->rowCount() === 1) {
-			return $resul->fetch();
-		} else {
-			return FALSE;
-		}
+	$resul = $bd->query($ins);
+	if ($resul->rowCount() === 1) {
+		return $resul->fetch();
+	} else {
+		return FALSE;
 	}
+}
 
 //otra funcion para registrar
+
+//estas funciones hay que incluirlas en bd_tienda_restaurantes.php
+function cargar_categorias()
+{
+	$res = leer_config(dirname(__FILE__) . "/configuracion.xml", dirname(__FILE__) . "/configuracion.xsd");
+	$bd = new PDO($res[0], $res[1], $res[2]);
+	$ins = "select codCat, nombre from categorias";
+	$resul = $bd->query($ins);
+	if (!$resul) {
+		return FALSE;
+	}
+	if ($resul->rowCount() === 0) {
+		return FALSE;
+	}
+	//si hay 1 o más
+	return $resul;
+}
+function cargar_categoria($codCat)
+{
+	$res = leer_config(dirname(__FILE__) . "/configuracion.xml", dirname(__FILE__) . "/configuracion.xsd");
+	$bd = new PDO($res[0], $res[1], $res[2]);
+	$ins = "select nombre, descripcion from categorias where codCat = $codCat";
+	$resul = $bd->query($ins);
+	if (!$resul) {
+		return FALSE;
+	}
+	if ($resul->rowCount() === 0) {
+		return FALSE;
+	}
+	//si hay 1 o más
+	return $resul->fetch();
+}
+
+function cargar_productos_categoria($codCat)
+{
+	$res = leer_config(dirname(__FILE__) . "/configuracion.xml", dirname(__FILE__) . "/configuracion.xsd");
+	$bd = new PDO($res[0], $res[1], $res[2]);
+	$sql = "select * from productos where categoria  = $codCat";
+	$resul = $bd->query($sql);
+	if (!$resul) {
+		return FALSE;
+	}
+	if ($resul->rowCount() === 0) {
+		return FALSE;
+	}
+	//si hay 1 o más
+	return $resul;
+}
+
+//comprobar si un producto ya está, si no está se añade, si está no se añade
+//buscar todo el rato en el carrito
+?>
--- Servidor/Ejercicios/Ejercicios_Clase/pedidos_restaurante/registro.php
+++ Servidor/Ejercicios/Ejercicios_Clase/pedidos_restaurante/registro.php
@@ -1,52 +1,61 @@
 <?php
+session_start();
 require_once 'bd_tienda.php';
 
+$errores = [];
+$correo = $clave = $pais = $cp = $ciudad = $direccion = "";
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     if (empty($_POST["correo"])) {
-        echo "Indique su correo.";
+        $errores[] = "Indique su correo.";
     } else {
-        $nombre = $_POST["correo"];
+        $correo = $_POST["correo"];
     }
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (empty($_POST["clave"])) {
-            echo "Indique su clave.";
-        } else {
-            $nombre = $_POST["clave"];
-        }
+    if (empty($_POST["clave"])) {
+        $errores[] = "Indique su clave.";
+    } else {
+        $clave = $_POST["clave"];
     }
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (empty($_POST["pais"])) {
-            echo "Indique su pais.";
-        } else {
-            $nombre = $_POST["pais"];
-        }
+    if (empty($_POST["pais"])) {
+        $errores[] = "Indique su pais.";
+    } else {
+        $pais = $_POST["pais"];
     }
 
     if (empty($_POST["cp"])) {
-        echo "Indique su cp.";
+        $errores[] = "Indique su cp.";
     } else {
-        $nombre = $_POST["cp"];
+        $cp = $_POST["cp"];
     }
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (empty($_POST["ciudad"])) {
-            echo "Indique su ciudad.";
-        } else {
-            $nombre = $_POST["ciudad"];
-        }
+    if (empty($_POST["ciudad"])) {
+        $errores[] = "Indique su ciudad.";
+    } else {
+        $ciudad = $_POST["ciudad"];
     }
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if (empty($_POST["direccion"])) {
-            echo "Indique su direccion.";
-        } else {
-            $nombre = $_POST["direccion"];
-        }
+    if (empty($_POST["direccion"])) {
+        $errores[] = "Indique su direccion.";
+    } else {
+        $direccion = $_POST["direccion"];
+    }
+
+    if (!empty($errores)) {
+
+    } else {
+        $_SESSION['correo'] = $correo;
+        $_SESSION['clave'] = $clave;
+        $_SESSION['pais'] = $pais;
+        $_SESSION['cp'] = $cp;
+        $_SESSION['ciudad'] = $ciudad;
+        $_SESSION['direccion'] = $direccion;
+        header("Location: login.php");
+        exit();
     }
 }
-
 ?>
 
 <!DOCTYPE html>
@@ -61,29 +70,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <?php if (isset($_GET["redirigido"])) {
         echo "<p>Inserte sus datos para continuar</p>";
     } ?>
-    <?php if (isset($err) and $err == true) {
-        echo "<p> Revise usuario y contraseña</p>";
+    <?php if (isset($errores) && !empty($errores)) {
+        foreach ($errores as $error) {
+            echo "<p>$error</p>";
+        }
     } ?>
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-        <label for="usuario">Correo</label>
-        <input value="<?php if (isset($correo))
-            echo $correo; ?>" id="correo" name="correo" type="text">
+        <label for="correo">Correo</label>
+        <input value="<?php echo isset($correo) ? $correo : ''; ?>" id="correo" name="correo" type="text">
         <label for="clave">Clave</label>
-        <input value="<?php if (isset($clave))
-            echo $clave; ?>" id="clave" name="clave" type="password">
+        <input value="<?php echo isset($clave) ? $clave : ''; ?>" id="clave" name="clave" type="password">
         <br><br><br>
-        <label for="usuario">Pais</label>
-        <input value="<?php if (isset($pais))
-            echo $clave; ?>" id="pais" name="pais" type="text">
-        <label for="clave">CP</label>
-        <input value="<?php if (isset($cp))
-            echo $cp; ?>" id="cp" name="cp" type="text">
-        <label for="usuario">Ciudad</label>
-        <input value="<?php if (isset($ciudad))
-            echo $ciudad; ?>" id="ciudad" name="ciudad" type="text">
-        <label for="clave">Direccion</label>
-        <input value="<?php if (isset($direccion))
-            echo $direccion; ?>" id="direccion" name="direccion" type="text">
+        <label for="pais">Pais</label>
+        <input value="<?php echo isset($pais) ? $pais : ''; ?>" id="pais" name="pais" type="text">
+        <label for="cp">CP</label>
+        <input value="<?php echo isset($cp) ? $cp : ''; ?>" id="cp" name="cp" type="text">
+        <label for="ciudad">Ciudad</label>
+        <input value="<?php echo isset($ciudad) ? $ciudad : ''; ?>" id="ciudad" name="ciudad" type="text">
+        <label for="direccion">Direccion</label>
+        <input value="<?php echo isset($direccion) ? $direccion : ''; ?>" id="direccion" name="direccion" type="text">
         <br><br><br>
         <input type="submit">
         <br><br><br>
